a4b92e19c8ae1527cbce82dd13839a98ccd3b00f
Maksim Buzikov <me.buzikov@physics.msu.ru>
2023-01-26 13:23:59 +0300

    Scipy dependence

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/shmax3/mmcs",
     packages=setuptools.find_packages(),
-    install_requires=['numba', 'numpy'],
+    install_requires=['numba', 'scipy', 'numpy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
